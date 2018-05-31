6c4f19f4837fb7edcb47c4c65892925b0b241c92
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-05-31 11:29:31 -0500

    add notes on leds

--- 00-tessel-intro/index.js
+++ 00-tessel-intro/index.js
@@ -2,9 +2,19 @@
 
 const tessel = require('tessel');
 
+// turn built-in led #2 on
+tessel.led[2].on();
+
+// turn built-in led #3 off (should be default)
+tessel.led[3].off();
+
+// alternate blinking of bothe led #2 and #3
 setInterval(() => {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
+	tessel.led[2].toggle();
+	tessel.led[3].toggle();
 }, 100);
 
+// to run on tessel, run
+//  t2 run 00-tessel-intro
+// in terminal
 console.log("I'm blinking! (Press CTRL + C to stop)");
--- 01-leds-and-buttons/01-blinking-led.js
+++ 01-leds-and-buttons/01-blinking-led.js
@@ -1,1 +1,24 @@
 // Our code here!
+// written with the johnny 5 library
+
+// set up board
+// require the johnny five language (there are different ones)
+const five = require('johnny-five');
+// require the tessel
+const Tessel = require('tessel-io');
+// connect the language to the tessel
+const board = new five.Board({
+	io: new Tessel()
+});
+
+// whenthe board is ready, run the function
+board.on('ready', () => {
+	// tell it where the led is! port a0
+	const led = new five.Led('a5');
+	// .blink takes a milliseconds argument
+	// led.blink(470);
+
+	// can't use port a0 for pulse. must use a PWM pin (ports 5 and 6 on the board)
+	// PWM = pulse width modulation
+	led.pulse(500, 1000);
+});
